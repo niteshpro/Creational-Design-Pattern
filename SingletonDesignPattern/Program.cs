0b7a2e3f9625a6e8dcf2cc0da86eb4f53b3d9d1b
Nitesh Sharma <niteshsharma9199@gmail.com>
2024-06-30 20:30:11 +0530

    initial commit for Single Design Pattern

---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,22 @@
+﻿using SingletonDesignPattern;
+using System;
+
+namespace DesignPattern
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Singleton Design Pattern");
+
+            Singleton EmployeeClass = Singleton.GetInstance;
+            EmployeeClass.PrintDetails("From Employee Class");
+
+            Singleton StudentClass = Singleton.GetInstance;
+            StudentClass.PrintDetails("From Student Class");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,39 @@
+﻿
+namespace SingletonDesignPattern
+{
+    public sealed class Singleton 
+        //we have used seal do this class can't be inherited
+        //and child class don't end up creting multiple obj or modify the constructor
+    {
+
+        private static int counter=0;
+        
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+            //this is a Property of Class Singleton which is having getter for the
+            //constructor
+        {
+            get {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                    
+                }
+                return instance;
+            }
+        
+        }
+
+        private Singleton()
+            //we have made the Constructor private so it can't be accessed directly from 
+            //other classes
+        {
+            counter++;
+            Console.WriteLine("Number of objects " + counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
