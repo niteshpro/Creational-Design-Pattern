a0cc40b02b572e3374a5778a20a6996ac141d5c5
Nitesh Sharma <niteshsharma9199@gmail.com>
2024-07-03 23:50:57 +0530

    Author: Nitesh Sharma Date: 03/07/2024 Reason: Implemented Lazy Loading with real life example of Logger

---
+++ LoggerSingleton/Log.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoggerSingleton
+{
+    public sealed class Log : ILog
+    {
+        private static readonly Lazy<Log> instance = new Lazy<Log>(() => new Log());
+        private Log() 
+        {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+            Console.WriteLine(currentTime);
+        }
+
+        public static Log GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void LogException(string message)
+        {
+            Console.WriteLine("------------------------------------------------------------------------------");
+            Console.WriteLine("Error Log: " + message);
+            Console.WriteLine("------------------------------------------------------------------------------");
+        }
+    }
+}
---
+++ LoggerSingleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LoggerSingleton
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Log Ilog = null;
+
+            int num2 = 0;
+
+            int GetDivide(int a, int b)
+            {
+                Ilog = Log.GetInstance;
+                //Obj for Log class only gets created at the time of demand
+                return a / b;
+            }
+
+            try
+            {
+                                
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                var result = GetDivide(num1, num2);
+                Console.WriteLine($"Multiplication Result: {result}");
+            }
+            catch (Exception ex)
+            {
+                Ilog.LogException(ex.ToString());
+            }
+        }
+    }
+}
